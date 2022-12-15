fd502ea78638a564d437505f8f189098fd3322f3
Yu1iann <kaizixy@gmail.com>
2022-12-15 21:17:18 +0300

    add all

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import output as out
 import input as imp
 import daypath as dp
 import nextday as nd
-#from variables import R, NA, xgamma, xalpha, xn
-#import functions as fun
+import time
+
+start = time.time() ## точка отсчета времени
 
 nday = 7  # период 7 дней
 nameday=['Monday','Tuesday ','Wednesday ','Thursday ','Friday','Saturday ','Sunday']
@@ -15,16 +16,12 @@ oldrow=2
 for g in range(14):  #14
     TP0,PARAM,nq, tq, TT,mr=imp.input1(rowpast)
     rowpast+=len(nq)
-
+    print(' Торговый представитель: ', TP0)
     #nq, tq, TT,mr=imp.input()
     n_idel=1 # для первого прогона цикла
     for i in range(nday):
         nda=i
-        print('_________DAY__',i+1,'_______ ')
-        #print('_________PARAM__', '_______ ')
-        #for k in range(4):
-            #print(PARAM[k])
-        #print(PARAM)
+        # print('_________DAY__',i+1,'_______ ')
         if n_idel == 0:
             first_TT = iposl + 1
         else:
@@ -39,14 +36,19 @@ for g in range(14):  #14
         out.output2(TP0,PARAM,nameday[nda], daypath, new_pathi, TT,oldrow)
         oldrow+=iposl+1
         if iposl==0:
-            print('__________len(path)________ ', len(path))
+            # print('__________len(path)________ ', len(path))
             if len(path)>1:
                 oldrow += 1
-            print('__________oldrow________ ', oldrow)
+            # print('__________oldrow________ ', oldrow)
             break
         tq, nq, TT, mr, n_idel = nd.nextday(iposl, new_pathi, nq, tq, TT, mr)
         path=new_pathi
-        print('__________oldrow________ ',oldrow)
-        print('__________________ ')
-        print(path, dist)
+        # print('__________oldrow________ ',oldrow)
+        #print('__________________ ')
+        # print(path, dist)
 out.clocwb()
+
+end = time.time() - start ## собственно время работы программы
+
+print(end ,' seconds') ## вывод времени сек
+print( end/60, ' min') ## вывод времени
